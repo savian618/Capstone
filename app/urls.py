242add920b1f6ce1483cf7aaebf5110e3c77c555
Savian Elam <savian.elam.18@cnu.edu>
2022-03-14 21:31:40 -0400

    project

---
+++ app/urls.py
@@ -0,0 +1,37 @@
+# pages/urls.py
+import feed
+from .views import HomePageView
+from feed.views import PostUpdateView, PostListView, UserPostListView
+from django.urls import path, include
+from profiles import views as profiles_views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path("", HomePageView.as_view(), name='home'),
+    path('feed/', PostListView.as_view(), name='feed'),
+    path('post/new/', feed.views.create_post, name='post-create'),
+    path('post/<int:pk>/', feed.views.post_detail, name='post-detail'),
+    path('like/', feed.views.like, name='post-like'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', feed.views.post_delete, name='post-delete'),
+    path('search_posts/', feed.views.search_posts, name='search_posts'),
+    path('user_posts/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('users/', profiles_views.users_list, name='users_list'),
+    path('users/<slug>/', profiles_views.profile_view, name='profile_view'),
+    path('friends/', profiles_views.friend_list, name='friend_list'),
+    path('users/friend-request/send/<int:id>/', profiles_views.send_friend_request, name='send_friend_request'),
+    path('users/friend-request/cancel/<int:id>/', profiles_views.cancel_friend_request, name='cancel_friend_request'),
+    path('users/friend-request/accept/<int:id>/', profiles_views.accept_friend_request, name='accept_friend_request'),
+    path('users/friend-request/delete/<int:id>/', profiles_views.delete_friend_request, name='delete_friend_request'),
+    path('users/friend/delete/<int:id>/', profiles_views.delete_friend, name='delete_friend'),
+    path('edit-profile/', profiles_views.edit_profile, name='edit_profile'),
+    path('my-profile/', profiles_views.my_profile, name='my_profile'),
+    path('search_users/', profiles_views.search_users, name='search_users'),
+    path('register/', profiles_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
+]
---
+++ profiles/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'SignUp!'))
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio', 'image', 'name']
